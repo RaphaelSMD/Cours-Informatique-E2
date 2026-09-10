96408164cb21cb6891134681789229eee366d859
Raphael Samoud <raphael.samoud@reseau.eseo.fr>
2026-09-10 08:54:01 +0200

    Ajout de l'exo 4

---
+++ tp1/exo4.py
@@ -0,0 +1,20 @@
+
+def approximation_pi(n):
+    if n < 0 :
+        print("Le nombre d'approximations doit être un entier positif.")
+        return
+    pi_approx = 3
+    print(f"Approximation 1 : {pi_approx}")
+
+    signe = 1
+    for i in range(1, n):
+        d = 2*i
+        terme = 4 / (d*(d+1)*(d+2))
+        pi_approx += signe * terme
+        print(f"Approximation {i+1} : {pi_approx}")
+        signe += -1
+try :
+    N=int(input("Entrer le nombre d'approximations voulues : "))
+    approximation_pi(N)
+except ValueError :
+    print("Entrer un nombre entier positif.")
