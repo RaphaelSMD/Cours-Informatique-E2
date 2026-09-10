fd0fb9cf7dc95654e33ae088930fca7c7cc49620
Raphael Samoud <raphael.samoud@reseau.eseo.fr>
2026-09-10 09:52:00 +0200

    Passage des exos 5/6/7 en fonction

--- tp1/exo5.py
+++ tp1/exo5.py
@@ -1,11 +1,13 @@
-nbr_decimal = int(input("Entrer un entier positif : "))
-if nbr_decimal == 0:
-    resultat = "0"
-else :
-    resultat =""
-    q = nbr_decimal
-    while q>0:
-        r = q%2
-        resultat = (str(r)+resultat)
-        q=q//2
-print(f"Le nombre {nbr_decimal} en binaire est : {resultat}")
+def conversion():
+    nbr_decimal = int(input("Entrer un entier positif : "))
+    if nbr_decimal == 0:
+        resultat = "0"
+    else:
+        resultat = ""
+        q = nbr_decimal
+        while q>0:
+            r = q%2
+            resultat = (str(r)+resultat)
+            q=q//2
+    print(f"Le nombre {nbr_decimal} en binaire est : {resultat}")
+conversion()
--- tp1/exo7.py
+++ tp1/exo7.py
@@ -1,17 +1,19 @@
-nombre =int(input("Saisissez le nombre de plaque d'immatriculation que vous voulez générer : "))
-import random
-lettres = "ABCDEFGHJKLMNPQRSTVWXYZ"
-chiffres = "0123456789"
-for i in range(nombre):
-    plaque = "SS"
-    while "SS" in plaque:
-        plaque=""
-        for j in range(2):
-            plaque += random.choice(lettres)
-        plaque += "-"
-        for j in range(3):
-            plaque += random.choice(chiffres)
-        plaque += "-"
-        for j in range(2):
-            plaque += random.choice(lettres)
-    print(plaque)
+def plaque_immatriculation():
+    nombre =int(input("Saisissez le nombre de plaque d'immatriculation que vous voulez générer : "))
+    import random
+    lettres = "ABCDEFGHJKLMNPQRSTVWXYZ"
+    chiffres = "0123456789"
+    for i in range(nombre):
+        plaque = "SS"
+        while "SS" in plaque:
+            plaque=""
+            for j in range(2):
+                plaque += random.choice(lettres)
+            plaque += "-"
+            for j in range(3):
+                plaque += random.choice(chiffres)
+            plaque += "-"
+            for j in range(2):
+                plaque += random.choice(lettres)
+        print(plaque)
+plaque_immatriculation()
