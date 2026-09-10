c4921ea100016ea69df45982847c9f7b1c6331f7
Raphael Samoud <raphael.samoud@reseau.eseo.fr>
2026-09-10 08:33:17 +0200

    Ajout de l'exo 3

---
+++ tp1/exo3.py
@@ -0,0 +1,9 @@
+humain=int(input("Entrer l'âge de l'humain : "))
+if humain < 0 :
+    print("l'âge ne peut pas être négatif.")
+else :
+    if humain <=2 :
+        canin = humain * 10.5
+    else :
+        canin = 21 + (humain - 2) * 4
+print(f"L'équivalent en âge canine est : {canin}")
