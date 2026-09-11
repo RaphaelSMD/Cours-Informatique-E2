40bd1a72277261f648150e9dc839e475e162cbce
Raphael Samoud <raphael.samoud@reseau.eseo.fr>
2026-09-11 13:51:46 +0200

    Passage en fonction des exos 2 et 3

--- tp1/exo2.py
+++ tp1/exo2.py
@@ -1,21 +1,23 @@
-nombres = []
-while True :
-    valeur = int(input("Entrer un nombre entier : "))
-    if valeur < 0 :
-        break
-    nombres.append(valeur)
+def rangement():
+    nombres = []
+    while True:
+        valeur = int(input("Entrer un nombre entier : "))
+        if valeur < 0:
+            break
+        nombres.append(valeur)
 
-if nombres :
-    nombres.sort()
+    if nombres :
+        nombres.sort()
 
-    minimum = nombres[0]
-    maximum = nombres[-1]
-    moyenne = sum(nombres) / len(nombres)
+        minimum = nombres[0]
+        maximum = nombres[-1]
+        moyenne = sum(nombres) / len(nombres)
 
-    print("\n--- Résultats ---")
-    print(f"Nombres triés : {nombres}")
-    print(f"Minimum : {minimum}")
-    print(f"Maximum : {maximum}")
-    print(f"Moyenne : {moyenne}")
-else :
-    print("Aucun nombre valide n'a été saisi.")
+        print("\n--- Résultats ---")
+        print(f"Nombres triés : {nombres}")
+        print(f"Minimum : {minimum}")
+        print(f"Maximum : {maximum}")
+        print(f"Moyenne : {moyenne}")
+    else :
+        print("Aucun nombre valide n'a été saisi.")
+rangement()
