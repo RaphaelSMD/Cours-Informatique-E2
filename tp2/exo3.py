ea479b5e198057636a82ea04944e0870f3ca473d
Raphael Samoud <raphael.samoud@reseau.eseo.fr>
2026-09-17 09:47:48 +0200

    Exercice 3 terminé

---
+++ tp2/exo3.py
@@ -0,0 +1,12 @@
+import random
+
+def verifier_tableau():
+    n = random.randint(3, 99)
+    tableau = [random.randint(0, 500) for _ in range(n)]
+    print(f"Tableau généré ({n} éléments) :", tableau)
+    if len(tableau) == len(set(tableau)):
+        return "Tous les éléments sont différents."
+    else:
+        return "Ils ne sont pas tous différents (il y a au moins un doublon)."
+
+print(verifier_tableau())
